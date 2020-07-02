2b812a28723d85b5fbeb89978289eb947d7cbfb2
Annina Elisabeth Lehmeyer <annina-elis.lehmeyer@hs-ansbach.de>
2020-07-02 11:53:55 +0200

    Ende3

---
+++ MyBread1/ReadMe.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// Die Elementvorlage "Leere Seite" wird unter https://go.microsoft.com/fwlink/?LinkId=234238 dokumentiert.
+
+namespace MyBread1
+{
+    /// <summary>
+    /// Eine leere Seite, die eigenständig verwendet oder zu der innerhalb eines Rahmens navigiert werden kann.
+    /// </summary>
+    public sealed partial class ReadMe : Page
+    {
+        public ReadMe()
+        {
+            this.InitializeComponent();
+        }
+        /*Autorin: Annina Lehmeyer 
+         * Matrikelnummer: 00154784
+         * 
+         * Quellen:
+         * Letzter Aufruf aller Webseiten: 22.06.2020
+         * - https://lebensmittel-warenkunde.de/lebensmittel/getreideprodukte/getreide-mehl/roggen-keime-getrocknet/roggen-mehl-typ-1150.html
+         * - https://www.gesundheitundwissenschaft.com/2016/12/warum-das-entzundungshemmende-basilikum.html#:~:text=Was%20sind%20die%20nachgewiesenen%20Gesundheitsvorteile%20von%20Basilikum%3F%201,7%20lebersch%C3%BCtzend%208%20gef%C3%A4%C3%9Fsch%C3%BCtzend%209%20stressreduzierend%2010%20abwehrst%C3%A4rkend
+         * - https://bessergesundleben.de/gesunde-koerner-und-samen-gehoeren-in-deine-ernaehrung/
+         * - https://www.zentrum-der-gesundheit.de/karotten.html
+         * - https://suessundclever.de/kamutmehl-eines-der-aeltesten-getreide-wird-neu-entdeckt/
+         * - https://gutemehle.info/dinkelmehl/
+         * - https://www.mymuesli.com/?utm_source=bing&utm_medium=cpc&utm_campaign=SEA-DE-BRAND-Ecosia-exclusive-Bing&utm_term=mym%C3%BCsli&utm_content=Brand%20exact
+         * - https://www.zentrum-der-gesundheit.de/kokosmehl-pi.html
+         * - https://www.bremer-gewuerzhandel.de/tomaten-getrocknet
+         * - https://de.fitnesslifestylehealthclub.com/gewichtsverlust/cranberry-gesundheitliche-vorteile-und-ernaehrung.html
+
+         
+         
+         */
+    }
+}
